e0195e23b847ed1167f203df2c5e868898853258
wsx25289 <10200860@zte.com.cn>
2017-10-26 20:36:12 +0800

    initial project struct

---
+++ configurator/impl/src/main/java/org/opendaylight/telemetry/impl/ConfiguratorProvider.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2017 ZTE, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.telemetry.impl;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConfiguratorProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ConfiguratorProvider.class);
+
+    private final DataBroker dataBroker;
+
+    public ConfiguratorProvider(final DataBroker dataBroker) {
+        this.dataBroker = dataBroker;
+    }
+
+    /**
+     * Method called when the blueprint container is created.
+     */
+    public void init() {
+        LOG.info("ConfiguratorProvider Session Initiated");
+    }
+
+    /**
+     * Method called when the blueprint container is destroyed.
+     */
+    public void close() {
+        LOG.info("ConfiguratorProvider Closed");
+    }
+}
